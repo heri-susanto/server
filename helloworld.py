7687e4c7f0b328a75c1757ac8adc3fdd004d3c41
Heri Susanto <susantoh41@gmail.com>
2019-08-15 00:03:39 +0700

    add json

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask import jsonify
+import json
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    d = 'Hello, World!'
-    return jsonify(d)
+    x = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+    y = json.dumps(x)
+    return jsonify(y)
